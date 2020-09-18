810823ac6f5491e39b489b0253e1840806df5b2b
Baboyma Kagniniwa <baboyma@gmail.com>
2020-09-18 11:41:13 -0400

    Initial COmmit

---
+++ Scripts/00_Config.R
@@ -0,0 +1,11 @@
+## PROJECT:  USAID/OHA HFR Data Processing
+## AUTHOR:   B.Kagniniwa | USAID
+## LICENSE:  MIT
+## PURPOSE:  Project Configuration
+## Date:     2020-09-18
+
+# GLOBALS
+
+dir_data <- "./Data"
+dir_dataout <- "./Dataout"
+
