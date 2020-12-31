66f6ea79d98f7c763290d9d5871c948b5d25b679
Baboyma Kagniniwa <baboyma@gmail.com>
2020-12-31 12:10:38 -0500

    Rwanda hfr sites list issues

--- Scripts/99_DDC_Mechs_Validations.R
+++ Scripts/99_DDC_Mechs_Validations.R
@@ -6,6 +6,7 @@
 ## LIBRARIES ----
 
   library(tidyverse)
+  library(readxl)
   library(Wavelength)
   library(glamr)
 
@@ -26,10 +27,16 @@
   # Site List
   sites <- "HFR_FY21_GLOBAL_sitelist_20201119.csv"
   
+  # Operatingunit
+  #cntry <- "Dominican Republic"
+  cntry <- "Rwanda"
+  
+  rwa_sites_file <- "HFR_FY21_SiteValidation_Rwanda_Revised_20201230.xlsx"
   
 ## DATA ----
 
-  # Site list
+  # Sites list
+  
   hfr_sites <- list.files(
       path = si_path(type = "path_datim"),
       pattern = sites,
@@ -39,14 +46,47 @@
   
   hfr_sites %>% glimpse()
   
+  # Check country sites
+  hfr_sites %>% 
+    filter(operatingunit == cntry) %>% 
+    glimpse()
+  
+  
+  rwa_sites_file %>% 
+    file.path("Data", .) %>% 
+    excel_sheets()
+  
+  sites_rwa <- rwa_sites_file %>% 
+    file.path("Data", .) %>% 
+    read_excel(sheet = 2)
+  
+  sites_rwa %>% glimpse()
+  
+  sites_rwa %>% 
+    is_ou_valid(orgs) %>% 
+    is_orgunituid_valid(orgs) %>% 
+    is_orgunituid4ou(orgs) %>% 
+    filter(valid_ou == FALSE | valid_uid == FALSE, uid_to_ou == FALSE) %>% 
+    glimpse() %>% 
+    View()
+  
+  sites_rwa %>% 
+    is_ou_valid(df_cntry_orgs) %>% 
+    is_orgunituid_valid(df_cntry_orgs) %>% 
+    is_orgunituid4ou(df_cntry_orgs) %>% 
+    filter(valid_ou == FALSE | valid_uid == FALSE, uid_to_ou == FALSE) %>% 
+    glimpse() %>% 
+    View()
+  
+  
   # OU Details
-  ou <- identify_ouuids(username = datim_user(),
+  ou <- glamr::identify_ouuids(username = datim_user(),
                         password = datim_pwd())
   
   ou %>% glimpse()
   
   ou_id <- ou %>% 
-    filter(country == "Dominican Republic") %>% 
+    filter(country == countryy) %>% 
     pull(uid)
   
   # Load lookups tables: ims, orgs, valid_dates
@@ -56,7 +96,7 @@
   mechanisms <- pull_mech() 
   
   # Data Orgs
-  df_cntry_orgs1 <- pull_hierarchy(ou_uid = ou_id,
+  df_cntry_orgs <- pull_hierarchy(ou_uid = ou_id,
                                  username = datim_user(),
                                  password = datim_pwd())
 
@@ -81,7 +121,8 @@
   # where mech_code in (17549, 81936);
   
   ## Mech codes ----
-  mechs <- c(17549, 81928, 81936)
+  #mechs <- c(17549, 81928, 81936) # PNG IMs
+  mechs <- c(16857, 16860, 81876)  # RWA IMs
   
   # Mechs from local file
   ims %>% 
@@ -99,30 +140,63 @@
     prinf()
   
   hfr_sites %>% 
-    filter(operatingunit %in% c("Eswatini", "Lesotho")) %>% View()
+    filter(operatingunit %in% cntry) %>% 
     distinct(operatingunit, mech_code) %>% 
     prinf()
   
   
   ## Orgunituids ----
-  orguids <- c("x3pmbo3X2Ln", "ltAqcBVf0mC")
-  orgunits <- c("Aid For Aids", "Hospital Alejo Martinez")
+  orguids <- c()
+  orgunits <- c("Gatsata health center",
+                "Legacy Clinic",
+                "La Medicale (kwa Kanimba) clinic",
+                "Rwampara health center",
+                "Nyabinyenga health center",
+                "Munyiginya health center")
 
   orgs %>% 
     filter(orgunituid %in% orguids) %>% 
     prinf()
   
   orgs %>% 
-    filter(str_detect(orgunit, paste0(orgunits, collapse = "|"))) %>% 
+    filter(
+      operatingunit == cntry,
+      str_detect(orgunit, paste0(orgunits, collapse = "|"))) %>% 
+    View()
+    prinf()
+    
+  orgs %>% 
+    filter(
+      operatingunit == cntry,
+      str_detect(str_to_lower(orgunit), 
+                 paste0(str_to_lower(orgunits), 
+                        collapse = "|"))) %>% 
+    View()
+    prinf()
+    
+  df_cntry_orgs %>% 
+    filter(
+      operatingunit == cntry,
+      str_detect(str_to_lower(orgunit), 
+                 paste0(str_to_lower(orgunits), 
+                        collapse = "|"))) %>% 
+    View()
     prinf()
   
   # Does not exist in local files
   hfr_orgunit_search(orgs, orgunit_name = orgunits[1])
   hfr_orgunit_search(orgs, orgunit_name = orgunits[2])
   
+  orgunits %>% 
+    map_dfc(.x, .f = ~ hfr_orgunit_search(df_cntry_orgs, orgunit_name = .x))
+  
   # Exist in Datim
-  df_cntry_orgs1 %>% 
+  df_cntry_orgs %>% 
     filter(orgunituid %in% orguids) %>% 
+    prinf()
+  
+  df_cntry_orgs %>% 
+    filter(orgunit %in% orgunits) %>% 
     prinf()
   
   df_orgs %>% 
